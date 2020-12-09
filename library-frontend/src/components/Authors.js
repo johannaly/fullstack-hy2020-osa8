43db882277b723e0db5a984da5e71af8bd3dc38b
johannaly <johanna@lyytinen.org>
2020-12-09 13:31:28 +0200

    kirjailijoiden näkymä 8.8

---
+++ library-frontend/src/components/Authors.js
@@ -0,0 +1,46 @@
+
+import React from 'react'
+
+
+const Authors = (props) => {
+  if (!props.show) {
+    return null
+  }
+
+  let authors = []
+  if (props.authors === undefined) {
+      authors = []
+      return null
+  } else {
+      authors = props.authors.allAuthors
+    
+    return (
+      <div>
+        <h2>authors</h2>
+        <table>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>
+                born
+            </th>
+              <th>
+                books
+            </th>
+            </tr>
+            {authors.map(a =>
+              <tr key={a.name}>
+                <td>{a.name}</td>
+                <td>{a.born}</td>
+                <td>{a.bookCount}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+
+      </div>
+    )
+  }
+}
+
+export default Authors
